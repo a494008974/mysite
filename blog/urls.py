13096471871b59bc105055347ed860a5dfac8343
zhou <494008974@qq.com>
2018-06-27 15:49:48 +0800

    aa

--- blog/urls.py
+++ blog/urls.py
@@ -18,4 +18,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('title/',views.blog_title)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from .models import BlogArticles
 # Create your views here.
 
 def index(request):
     return render(request,'blog/index.html')
+
+
+def blog_title(request):
+    blogs = BlogArticles.objects.all()
+    return render(request, 'blog/titles.html', {'blogs':blogs})
